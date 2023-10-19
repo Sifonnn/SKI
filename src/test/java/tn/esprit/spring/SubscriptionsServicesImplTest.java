1c941294639e6e3d03adee0e98a1b85dd4710db4
DangledPointers <128555585+DangledPointers@users.noreply.github.com>
2023-10-19 21:08:24 +0100

    JUnit tests + Mockito mocking + Application.properties adjustments

---
+++ src/test/java/tn/esprit/spring/SubscriptionsServicesImplTest.java
@@ -0,0 +1,98 @@
+package tn.esprit.spring;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.entities.Subscription;
+import tn.esprit.spring.entities.TypeSubscription;
+import tn.esprit.spring.repositories.ISubscriptionRepository;
+import tn.esprit.spring.services.SubscriptionServicesImpl;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+class SubscriptionServicesImplTest {
+
+    @Mock
+    private ISubscriptionRepository subscriptionRepository;
+
+    @InjectMocks
+    private SubscriptionServicesImpl subscriptionServices;
+
+    @Test
+    void testAddSubscription() {
+        Subscription subscription = new Subscription();
+        subscription.setTypeSub(TypeSubscription.ANNUAL);
+        subscription.setStartDate(LocalDate.now());
+
+        when(subscriptionRepository.save(subscription)).thenReturn(subscription);
+
+        Subscription addedSubscription = subscriptionServices.addSubscription(subscription);
+        assertEquals(subscription, addedSubscription);
+
+        verify(subscriptionRepository, times(1)).save(subscription);
+    }
+
+    @Test
+    void testUpdateSubscription() {
+        Subscription subscription = new Subscription();
+
+        when(subscriptionRepository.save(subscription)).thenReturn(subscription);
+
+        Subscription updatedSubscription = subscriptionServices.updateSubscription(subscription);
+        assertEquals(subscription, updatedSubscription);
+
+        verify(subscriptionRepository, times(1)).save(subscription);
+    }
+
+    @Test
+    void testRetrieveSubscriptionById() {
+        Long subscriptionId = 1L;
+
+        Subscription subscription = new Subscription();
+        when(subscriptionRepository.findById(subscriptionId)).thenReturn(Optional.of(subscription));
+
+        Subscription retrievedSubscription = subscriptionServices.retrieveSubscriptionById(subscriptionId);
+        assertEquals(subscription, retrievedSubscription);
+
+        verify(subscriptionRepository, times(1)).findById(subscriptionId);
+    }
+
+    @Test
+    void testGetSubscriptionByType() {
+        TypeSubscription type = TypeSubscription.ANNUAL;
+
+        Set<Subscription> subscriptions = new HashSet<>();
+        when(subscriptionRepository.findByTypeSubOrderByStartDateAsc(type)).thenReturn(subscriptions);
+
+        Set<Subscription> retrievedSubscriptions = subscriptionServices.getSubscriptionByType(type);
+        assertEquals(subscriptions, retrievedSubscriptions);
+
+        verify(subscriptionRepository, times(1)).findByTypeSubOrderByStartDateAsc(type);
+    }
+
+    @Test
+    void testRetrieveSubscriptionsByDates() {
+        LocalDate startDate = LocalDate.of(2023, 1, 1);
+        LocalDate endDate = LocalDate.of(2023, 1, 31);
+
+        List<Subscription> subscriptions = List.of(new Subscription(), new Subscription());
+        when(subscriptionRepository.getSubscriptionsByStartDateBetween(startDate, endDate)).thenReturn(subscriptions);
+
+        List<Subscription> retrievedSubscriptions = subscriptionServices.retrieveSubscriptionsByDates(startDate, endDate);
+        assertEquals(subscriptions, retrievedSubscriptions);
+
+        verify(subscriptionRepository, times(1)).getSubscriptionsByStartDateBetween(startDate, endDate);
+    }
+}
