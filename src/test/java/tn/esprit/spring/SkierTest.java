563863091a6aa4ca039d85951e19aca40de19a35
amine <47643041+ravan5254@users.noreply.github.com>
2023-10-25 01:10:08 +0200

    Update SkierTest.java

--- src/test/java/tn/esprit/spring/SkierTest.java
+++ src/test/java/tn/esprit/spring/SkierTest.java
@@ -1,2 +1,56 @@
-package tn.esprit.spring;public class SkierTest {
+package tn.esprit.spring;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import tn.esprit.spring.entities.Skier;
+import tn.esprit.spring.repositories.ISkierRepository;
+import tn.esprit.spring.services.SkierServicesImpl;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class SkierTest {
+
+    @Mock
+    private ISkierRepository skierRepository;
+
+    @InjectMocks
+    private SkierServicesImpl skierServices;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testRetrieveSkier() {
+        // Arrange
+        Long skierId = 1L;
+        Skier skier = new Skier();
+        when(skierRepository.findById(skierId)).thenReturn(Optional.of(skier));
+
+        // Act
+        Skier retrievedSkier = skierServices.retrieveSkier(skierId);
+
+        // Assert
+        assertEquals(skier, retrievedSkier);
+        verify(skierRepository, times(1)).findById(skierId);
+    }
+
+    @Test
+    void testRemoveSkier() {
+        // Arrange
+        Long skierId = 1L;
+
+        // Act
+        skierServices.removeSkier(skierId);
+
+        // Assert
+        verify(skierRepository, times(1)).deleteById(skierId);
+    }
 }
