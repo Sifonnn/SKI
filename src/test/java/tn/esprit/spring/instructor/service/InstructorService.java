3e6aa392efcb17def59cc2cbe5fe30311cef9fb2
wejden11 <wejden.barhoumi@esprit.tn>
2023-11-05 00:50:04 +0100

    src update

--- src/test/java/tn/esprit/spring/instructor/service/InstructorService.java
+++ src/test/java/tn/esprit/spring/instructor/service/InstructorService.java
@@ -1,8 +1,8 @@
 package tn.esprit.spring.instructor.service;
 
-import org.junit.Test;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
--- src/test/java/tn/esprit/spring/instructor/service/InstructorServicesImplTest.java
+++ src/test/java/tn/esprit/spring/instructor/service/InstructorServicesImplTest.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
