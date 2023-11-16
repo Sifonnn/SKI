ef7fdfa0306f6046b5a9c63a5d925d0b1fe16f77
amine <47643041+ravan5254@users.noreply.github.com>
2023-11-16 16:19:45 +0100

    Update IRegistrationRepository.java

--- src/main/java/tn/esprit/spring/repositories/IRegistrationRepository.java
+++ src/main/java/tn/esprit/spring/repositories/IRegistrationRepository.java
@@ -16,7 +16,7 @@ public interface IRegistrationRepository extends CrudRepository<Registration, Lo
             "on reg.course member ins.courses " +
             "where ins.numInstructor = :idIns and reg.course.support = :support")
     List<Integer> numWeeksCourseOfInstructorBySupport(@Param("idIns") Long numInstructor, @Param("support") Support support);
-
+/*
     @Query("select count(distinct r) from Registration r " +
             "where r.numWeek = ?1 and r.skier.numSkier = ?2 and r.course.numCourse = ?3")
     long countDistinctByNumWeekAndSkier_NumSkierAndCourse_NumCourse(int numWeek, Long numSkier, Long numCourse);
@@ -24,7 +24,7 @@ public interface IRegistrationRepository extends CrudRepository<Registration, Lo
   //  long countDistinctByNumWeekAndSkier_NumSkierAndCourse_NumCourse(int numWeek, Long numSkier, Long numCourse);
 
 
-
+*/
 
 
 
