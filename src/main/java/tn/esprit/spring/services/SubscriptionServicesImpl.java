b94827847368e49e85709da94580a595d6024df9
DangledPointers <128555585+DangledPointers@users.noreply.github.com>
2023-11-16 21:35:31 +0100

    Fixes

--- src/main/java/tn/esprit/spring/services/SubscriptionServicesImpl.java
+++ src/main/java/tn/esprit/spring/services/SubscriptionServicesImpl.java
@@ -70,11 +70,20 @@ public class SubscriptionServicesImpl implements ISubscriptionServices{
     }
 
    // @Scheduled(cron = "* 0 9 1 * *") /* Cron expression to run a job every month at 9am */
-    @Scheduled(cron = "*/30 * * * * *") /* Cron expression to run a job every 30 secondes */
-    public void showMonthlyRecurringRevenue() {
-        Float revenue = subscriptionRepository.recurringRevenueByTypeSubEquals(TypeSubscription.MONTHLY)
-                + subscriptionRepository.recurringRevenueByTypeSubEquals(TypeSubscription.SEMESTRIEL)/6
-                + subscriptionRepository.recurringRevenueByTypeSubEquals(TypeSubscription.ANNUAL)/12;
-        log.info("Monthly Revenue = " + revenue);
-    }
+@Scheduled(cron = "*/30 * * * * *") /* Cron expression to run a job every 30 seconds */
+public void showMonthlyRecurringRevenue() {
+    Float monthlyRevenue = subscriptionRepository.recurringRevenueByTypeSubEquals(TypeSubscription.MONTHLY);
+    Float semestrialRevenue = subscriptionRepository.recurringRevenueByTypeSubEquals(TypeSubscription.SEMESTRIEL);
+    Float annualRevenue = subscriptionRepository.recurringRevenueByTypeSubEquals(TypeSubscription.ANNUAL);
+
+    // Handling null values by providing default values (0.0f)
+    monthlyRevenue = (monthlyRevenue != null) ? monthlyRevenue : 0.0f;
+    semestrialRevenue = (semestrialRevenue != null) ? semestrialRevenue : 0.0f;
+    annualRevenue = (annualRevenue != null) ? annualRevenue : 0.0f;
+
+    Float revenue = monthlyRevenue + semestrialRevenue / 6 + annualRevenue / 12;
+
+    log.info("Monthly Revenue = " + revenue);
+}
+
 }
